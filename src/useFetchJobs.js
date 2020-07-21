1902d95f2ae63f527c73fbb3310ec51493133f91
Jexxie <1989wjx@gmail.com>
2020-07-21 12:04:40 +0800

    add pagination

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,6 +5,7 @@ const ACTIONS = {
 	MAKE_REQUEST: 'make-request',
 	GET_DATA: 'get-data',
 	ERROR: 'error',
+	UPDATE_HAS_NEXT_PAGE: 'update-has-next-page',
 }
 
 function reducer(state, action) {
@@ -15,6 +16,8 @@ function reducer(state, action) {
 			return { ...state, loading: false, jobs: action.payload.jobs }
 		case ACTIONS.ERROR:
 			return { ...state, loading: false, error: action.payload.error, jobs: [] }
+		case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+			return { ...state, hasNextPage: action.payload.hasNextPage }
 		default:
 			return state
 	}
@@ -36,11 +39,11 @@ export default function useFetchJobs(params, page) {
 	})
 
 	useEffect(() => {
-		const cancelToken = axios.CancelToken.source()
+		const cancelToken1 = axios.CancelToken.source()
 		dispatch({ type: ACTIONS.MAKE_REQUEST })
 		axios
 			.get(URL, {
-				cancelToken: cancelToken.token,
+				cancelToken: cancelToken1.token,
 				params: { markdown: true, page: page, ...params },
 			})
 			.then((res) => {
@@ -52,8 +55,28 @@ export default function useFetchJobs(params, page) {
 				dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
 			})
 
+		// send another request to check if nextpage exist
+		const cancelToken2 = axios.CancelToken.source()
+		axios
+			.get(URL, {
+				cancelToken: cancelToken2.token,
+				params: { markdown: true, page: page + 1, ...params },
+			})
+			.then((res) => {
+				dispatch({
+					type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
+					payload: { hasNextPage: res.data.length !== 0 },
+				})
+			})
+			.catch((e) => {
+				// ignore cancel error, only check errors that occur not because of canceling
+				if (axios.isCancel(e)) return
+				dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+			})
+
 		return () => {
-			cancelToken.cancel()
+			cancelToken1.cancel()
+			cancelToken2.cancel()
 		}
 	}, [params, page])
 
