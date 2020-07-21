6ca6ca80987fbd5ac4300e8d847ead8168cdaa4c
Jexxie <1989wjx@gmail.com>
2020-07-21 14:13:03 +0800

    add searchform for description, location, full time

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import useFetchJobs from './useFetchJobs'
 import { Container } from 'react-bootstrap'
 import Job from './components/Job'
 import JobsPagination from './components/JobsPagination'
+import SearchForm from './components/SearchForm'
 
 function App() {
 	const [params, setParams] = useState({})
 	const [page, setPage] = useState(1)
 	const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
+	function handleParamChange(e) {
+		const param = e.target.name
+		const value = e.target.value
+		setPage(1)
+		setParams((prevParams) => {
+			return { ...prevParams, [param]: value }
+		})
+	}
+
 	return (
 		<Container className="my-4">
 			<h1 className="mb-4">GitHub Jobs</h1>
+			<SearchForm params={params} onParamChange={handleParamChange} />
 			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
 			{loading && <h1>Loading...</h1>}
 			{error && <h1>Error. Try Refreshing.</h1>}
